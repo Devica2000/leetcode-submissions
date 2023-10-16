995fcc6365430f0f85160efe56c99801207a5ff9
Devica2000 <63335835+Devica2000@users.noreply.github.com>
2023-10-15 23:12:30 -0700

    Time: 472 ms (69.84%), Space: 30.7 MB (86.84%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,36 +1,18 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
 
-        # Use a set since set stores the unique elements
-        # Check the length of the set and the list
-        # if they are equal -> all elements are unique
-        # if not, there are duplicates
-        # Linear time algo 
-        # converting list -> O(n)
-
-        num_set = set(nums) 
-
-        #O(1)
-        if len(nums) == len(num_set): 
-            return False
-        else:
-            return True
-
-        #Brute force - O(n2)
-        # if len(nums) == 1:
-        #     return False
-        # for i in range(len(nums)):
-        #     for j in range(i+1, len(nums)):
-        #         if nums[i] == nums[j]:
-        #             return True
-        # return False
-
-        # Using hashSet - Neetcode solution
         # O(n)
+        hashnum = set(nums)
+        if len(nums) == len(hashnum):
+            return False
+        return True
 
-        # hashset = set()
-        # for n in nums:
-        #     if n in hashset:
-        #         return True
-        #     hashset.add(n)
-        # return False
+        # the process of converting list nums to set expanded
+        # O(n)
+        hashset = set()
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
+        
