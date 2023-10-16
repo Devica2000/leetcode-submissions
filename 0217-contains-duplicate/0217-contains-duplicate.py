84c4aa3ec49d9a6716afb151410387ae0a43b222
Devica2000 <63335835+Devica2000@users.noreply.github.com>
2023-10-15 23:12:59 -0700

    Time: 470 ms (75.54%), Space: 30.9 MB (74.52%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -2,17 +2,17 @@ class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
 
         # O(n)
-        hashnum = set(nums)
-        if len(nums) == len(hashnum):
-            return False
-        return True
+        # hashnum = set(nums)
+        # if len(nums) == len(hashnum):
+        #     return False
+        # return True
 
         # the process of converting list nums to set expanded
         # O(n)
-        # hashset = set()
-        # for n in nums:
-        #     if n in hashset:
-        #         return True
-        #     hashset.add(n)
-        # return False
+        hashset = set()
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
         
