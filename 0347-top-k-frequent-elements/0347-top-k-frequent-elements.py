49ec1b99573baf8c62664c08a86de6fe6a87172b
Devica2000 <63335835+Devica2000@users.noreply.github.com>
2023-10-19 18:25:11 -0700

    Time: 105 ms (29.71%), Space: 21.9 MB (26.44%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,13 +1,34 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        countmap = {}
 
-        for num in nums:
-            if num not in countmap:
-                countmap[num] = 1
-            else:
-                countmap[num] += 1
+        # Linear Time - O(n)
+        count = {}  #hashmap to keep track of the count of elements
+        freq = [[] for i in range(len(nums) + 1)] #bucket sort special array
+
+        for n in nums:
+            count[n] = 1 + count.get(n,0)
+        for n,c in count.items():
+            freq[c].append(n)
         
-        sorted_dict = dict(sorted(countmap.items(), key = lambda x: x[1], reverse =True))
-        output_keys = list(sorted_dict.keys())
-        return output_keys[:k]
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+        # O(nlogn)
+        # countmap = {}
+
+        # for num in nums:
+        #     countmap[num] = 1+ countmap.get(num, 0)
+        #     # if num not in countmap:
+        #     #     countmap[num] = 1
+        #     # else:
+        #     #     countmap[num] += 1
+        
+        # sorted_dict = dict(sorted(countmap.items(), key = lambda x: x[1], reverse =True))
+        # output_keys = list(sorted_dict.keys())
+        # return output_keys[:k]
+
+
