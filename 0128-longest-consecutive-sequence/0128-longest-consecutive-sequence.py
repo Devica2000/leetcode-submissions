e165449df698458a779e5c93f368c79c0a4d3cd0
Devica2000 <63335835+Devica2000@users.noreply.github.com>
2023-10-22 23:02:12 -0700

    Time: 6494 ms (5.01%), Space: 31.2 MB (64.53%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,41 +1,33 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        # Using set 
-        numSet = set(nums)
-        longest = 0
+        # Using set - O(n)
+        # numSet = set(nums)
+        # longest = 0
         
-        for n in nums:
-            #check the left value to see if it is the start of the sequence
-            if (n-1) not in numSet:
-                length = 0
+        # for n in nums:
+        #     #check the left value to see if it is the start of the sequence
+        #     if (n-1) not in numSet:
+        #         length = 0
+        #         while (n + length) in numSet:
+        #             length += 1
+        #         longest = max(longest, length)
+        # return longest
 
-                while (n + length) in numSet:
-                    length += 1
-                
-                longest = max(longest, length)
+        if not nums:
+            return 0
 
-        return longest
+        num_set = set(nums)
+        longest_streak = 0
 
+        for num in nums:
+            if num - 1 not in num_set:
+                current_num = num
+                current_streak = 1
 
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_streak += 1
 
-
-
-        # if not nums:
-        #     return 0
-
-        # num_set = set(nums)
-        # longest_streak = 0
-
-        # for num in num_set:
-        #     if num - 1 not in num_set:
-        #         current_num = num
-        #         current_streak = 1
-
-        #         while current_num + 1 in num_set:
-        #             current_num += 1
-        #             current_streak += 1
-
-        #         longest_streak = max(longest_streak, current_streak)
+                longest_streak = max(longest_streak, current_streak)
         
-        # return longest_streak
-                
+        return longest_streak
