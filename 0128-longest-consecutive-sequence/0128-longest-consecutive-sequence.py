6ac0b76b6270fa2d6692cb2674a907d746a4b83a
Devica2000 <63335835+Devica2000@users.noreply.github.com>
2023-10-22 23:02:31 -0700

    Time: 398 ms (53.65%), Space: 31.1 MB (64.53%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -19,7 +19,7 @@ class Solution:
         num_set = set(nums)
         longest_streak = 0
 
-        for num in nums:
+        for num in num_set:
             if num - 1 not in num_set:
                 current_num = num
                 current_streak = 1
