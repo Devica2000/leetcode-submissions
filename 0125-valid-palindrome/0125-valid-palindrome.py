e8de340557e88f5351582c716af1fc6bc0543317
Devica2000 <63335835+Devica2000@users.noreply.github.com>
2023-10-24 00:13:21 -0700

    Time: 77 ms (5.41%), Space: 17.2 MB (50.68%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,13 +1,35 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # Reverse a string
-        new_str = ""
+        # Time Complexity = O(n)
+        # Space Complexity - O(1)
+        l = 0
+        r = len(s) - 1
 
-        for c in s:
-            if c.isalnum():
-                new_str += c.lower()
+        while l < r:
+            while l < r and not self.alnum(s[l]):
+                l += 1
+            while r > l and not self.alnum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
+
+        # helper function to check if a character is alphanumeric
+    def alnum(self, c):
+        return ((ord('A') <= ord(c) <= ord('Z')) or 
+        (ord('a') <= ord(c) <= ord('z')) or
+        (ord('0') <= ord(c) <= ord('9')))
+        # Reverse a string
+        # # Time and space Complexity = O(n)
+        # new_str = ""
+
+        # for c in s:
+        #     if c.isalnum():
+        #         new_str += c.lower()
         
-        return new_str == new_str[::-1]
+        # return new_str == new_str[::-1]
 
 
         # Time & Space Complexity = O(n)
