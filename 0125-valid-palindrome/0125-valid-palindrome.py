468e4fd2fe8d7635da80fd197b1397c59d264d96
Devica2000 <63335835+Devica2000@users.noreply.github.com>
2023-10-24 00:05:20 -0700

    Time: 53 ms (42.88%), Space: 17 MB (92.39%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,26 +1,32 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        if s.isspace() == True:
-            return True
-        s = s.lower()
+        # Reverse a string
         new_str = ""
-        for char in s:
-            if char.isalnum() == True:
-                new_str += char
-            else:
-                continue
 
-        l = 0
-        r = len(new_str) - 1
+        for c in s:
+            if c.isalnum():
+                new_str += c.lower()
+        
+        return new_str == new_str[::-1]
 
-        while l < r:
-            if new_str[l] == new_str[r]:
-                l += 1
-                r -= 1
-            else:
-                return False
-        return True
 
-            
+        # Time & Space Complexity = O(n)
+        # if s.isspace() == True:
+        #     return True
+        # new_str = ""
+        # for char in s:
+        #     if char.isalnum():
+        #         new_str += char.lower()
+        #     else:
+        #         continue
 
-       
+        # l = 0
+        # r = len(new_str) - 1
+
+        # while l < r:
+        #     if new_str[l] == new_str[r]:
+        #         l += 1
+        #         r -= 1
+        #     else:
+        #         return False
+        # return True 
