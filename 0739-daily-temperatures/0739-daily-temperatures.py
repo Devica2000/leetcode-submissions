5d59128cc26fa9786be1d217a86791119db724c7
Devica2000 <63335835+Devica2000@users.noreply.github.com>
2023-10-29 15:33:59 -0700

    Time: 1055 ms (69.38%), Space: 32.6 MB (14.73%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,42 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        # O(n) time and space complexity
+        # result - to store the wait days at ith position
+        # stack - to add pair of temp, idx from temperatures
+
+        res = [0] * len(temperatures)
+        stack = []
+
+        for idx, temp in enumerate(temperatures):
+            # check if the stack is non empty
+            # then check if the last added element
+            # is smaller than the current element
+            while stack and temp > stack[-1][0]:
+                stackTemp, stackIdx = stack.pop()
+                res[stackIdx] = idx - stackIdx
+            stack.append([temp,idx])
+        return res
+
+        #brute force - O(n^2)
+        # wait_days = [0] * len(temperatures)
+        # for i in range(len(temperatures)):
+        #     for j in range(i+1, len(temperatures)):
+        #         if temperatures[j] > temperatures[i]:
+        #             days = j - i
+        #             break
+        #         else:
+        #             days = 0
+        #     if i == len(temperatures) -1:
+        #         days = 0
+        #     wait_days[i] = days
+        # return wait_days
+        
+        # O(n^2) and O(1)
+        # for i in range(len(temperatures)):
+        #     days = 0
+        #     for j in range(i+1, len(temperatures)):
+        #         if temperatures[j] > temperatures[i]:
+        #             days = j - i
+        #             break
+        #     temperatures[i] = days
+        # return temperatures
