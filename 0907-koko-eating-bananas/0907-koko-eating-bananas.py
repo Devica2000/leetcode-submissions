8cb762de73eef0c333988a82bce88f548846bb87
Devica2000 <63335835+Devica2000@users.noreply.github.com>
2023-11-02 17:34:56 -0700

    Time: 381 ms (28.93%), Space: 17.9 MB (19.4%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        #Time Complexity = O(log(max p) * p)
+        #Given: len(piles) <= h  
+        #Koko only eats at most 1 pile of bananas in 1 hour
+        #min k = 1
+        #To finish everything in <= h hours, Max k = max(piles)
+
+        l = 1
+        r = max(piles)
+        res = r
+
+        while l <= r:
+            k = (l + r)//2
+            hours = 0
+            for p in piles:
+                hours += math.ceil(p/k)
+            if hours <= h:
+                res = min(res,k)
+                r = k - 1
+            else:
+                l = k + 1
+        return res
+
+        #brute force - O(n^2)
+        # for num in range(1, max(piles) + 1):
+        #     hours = 0
+        #     for p in piles:
+        #         hours += math.ceil(p/num)
+        #     if hours <= h:
+        #         return num
+
+
+        
