dd07f239d43509ef9c1def9d2c2d1917ff83731b
Devica2000 <63335835+Devica2000@users.noreply.github.com>
2024-10-01 22:57:30 -0700

    Time: 46 ms (98.36%), Space: 17.8 MB (34.4%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,18 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     for i in range(len(nums)):
+    #         for j in range(i+1, len(nums)):
+    #             if nums[i] + nums[j] == target:
+    #                 return i, j
 
+        idx_val = {}
+        for idx, num in enumerate(nums):
+            diff = target - num
+            if diff in idx_val:
+                return(idx_val[diff], idx)
+            idx_val[num] = idx
+        return
+                
         #Brute Force - O(n^2)
         # for i in range(len(nums)):
         #     diff = target - nums[i]
@@ -17,10 +29,10 @@ class Solution:
         #Using hashmap
         # Time becomes linear - O(n)
 
-        idx_val = {} #hashmap to store the val:idx pairs
-        for idx, num in enumerate(nums):
-            diff = target - num
-            if diff in idx_val:
-                return [idx_val[diff], idx]
-            idx_val[num] = idx
-        return #not necessary since we are guaranteed that a solution exists
+        # idx_val = {} #hashmap to store the val:idx pairs
+        # for idx, num in enumerate(nums):
+        #     diff = target - num
+        #     if diff in idx_val:
+        #         return [idx_val[diff], idx]
+        #     idx_val[num] = idx
+        # return #not necessary since we are guaranteed that a solution exists
