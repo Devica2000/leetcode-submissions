0c4ba18824a4dbcd3c7ee42627f1c0d187e53323
Devica2000 <63335835+Devica2000@users.noreply.github.com>
2024-10-02 23:39:13 -0700

    Time: 408 ms (89.4%), Space: 34.6 MB (16.68%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,18 +1,83 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        # Solution 1: Using a set()
+        # if len(set(nums)) != len(nums):
+        #     return True
+        # return False
 
-        # O(n)
-        # hashnum = set(nums)
-        # if len(nums) == len(hashnum):
-        #     return False
-        # return True
+        # Solution 2: Using a hashmap
+        # Hashmap to store num:count pairs
+        # num_counts = {} 
 
-        # the process of converting list nums to set expanded
-        # O(n)
-        hashset = set()
-        for n in nums:
-            if n in hashset:
+        # for num in nums:
+        #     if num not in num_counts:
+        #         num_counts[num] = 1
+        #     else:
+        #         num_counts[num] += 1
+
+        # for count in num_counts.values():
+        #     if count > 1:
+        #         return True
+        # return False
+
+        # # Using hashmap
+        # count = {}
+        # for n in nums:
+        #     if n in count:
+        #         return True
+        #     else:
+        #         count[n] = True
+        # return False
+
+        #Brute force
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] == nums[j]:
+        #             return True
+        # return False
+
+        # Using sorting - O(nlogn) and O(1)
+        # nums = sorted(nums)
+
+        # for i in range(len(nums)-1):
+        #     if nums[i] == nums[i+1]:
+        #         return True
+        # return False
+
+        # Using a hashmap
+        # O(n) time and space
+        val_count = {}
+
+        for num in nums:
+            if num not in val_count:
+                val_count[num] = True
+            else:
                 return True
-            hashset.add(n)
         return False
-        
+
+        # Keep a count of the elements
+        # O(n) time and O(n) space
+        # O(n) - worst case store all elements in case of no duplicates
+
+        # val_count = {}
+
+        # for num in nums:
+        #     if num not in val_count:
+        #         val_count[num] = 1
+        #     else:
+        #         val_count[num] += 1
+
+        # for count in val_count.values():
+        #     if count > 1:
+        #         return True
+        # return False
+
+        # Using hashset -  O(n) time and space
+
+        # hashset = set()
+
+        # for num in nums:
+        #     if num in hashset:
+        #         return True
+        #     hashset.add(num)
+        # return False
