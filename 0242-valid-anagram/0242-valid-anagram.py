4fbd84ec8cdf7e5f547b65c8d27d32ef6e5fb022
Devica2000 <63335835+Devica2000@users.noreply.github.com>
2024-10-03 22:55:38 -0700

    Time: 56 ms (21.39%), Space: 16.9 MB (52.95%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,36 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+
+        count_s = {}
+        count_t = {}
+
+        for elem in s:
+            if elem not in count_s:
+                count_s[elem] = 1
+            else:
+                count_s[elem] += 1
+
+        for val in t:
+            if val not in count_t:
+                count_t[val] = 1
+            else:
+                count_t[val] += 1
+
+        if count_s == count_t:
+            return True
+        return False
+
+        # # Compare counts
+        # for key in count_s:
+        #     if key not in count_t or count_s[key] != count_t[key]:
+        #         return False
+        
+        # # If all keys match, return True
+        # return True
+
         # if sorted(s) == sorted(t):
         #     return True
         # else:
@@ -16,6 +47,7 @@ class Solution:
 
         countS, countT = {}, {}
 
+        #Use get to avoid key not found error
         for i in range(len(s)):
             countS[s[i]] = 1 + countS.get(s[i], 0)
             countT[t[i]] = 1 + countT.get(t[i], 0)
