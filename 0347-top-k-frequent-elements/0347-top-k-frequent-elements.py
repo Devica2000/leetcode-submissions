d34740e01c2f56bf47c9022c85b422e6f12497c4
Devica2000 <63335835+Devica2000@users.noreply.github.com>
2024-10-05 23:23:02 -0700

    Time: 93 ms (33.68%), Space: 22 MB (16.53%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,18 +1,50 @@
 class Solution:
+    import heapq
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #Using Bucket Sort
 
-        num_count = {}
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
 
         for num in nums:
-            if num not in num_count:
-                num_count[num] = 1
-            else:
-                num_count[num] += 1
+            count[num] = 1 + count.get(num, 0)
+        for num, c in count.items():
+            freq[c].append(num)
+
+        res = []
+
+        #we need to look for numbers from the end to the start (desc order)
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+            if len(res) == k:
+                return res
+
+
+
+        # num_count = {}
+
+        # for num in nums:
+        #     if num not in num_count:
+        #         num_count[num] = 1
+        #     else:
+        #         num_count[num] += 1
         
-        sorted_counts = dict(sorted(num_count.items(), key=lambda item: item[1], reverse = True))
-        return list(sorted_counts.keys())[:k]
+        # sorted_counts = dict(sorted(num_count.items(), key=lambda item: item[1], reverse = True))
+        # return list(sorted_counts.keys())[:k]
 
-
+        # count = Counter(nums)
+        # # Use a max heap to get the k most frequent elements
+        # # Since heapq is a min-heap, we can use negative frequencies
+        # max_heap = [(-freq, num) for num, freq in count.items()]
+        # # Create the heap
+        # heapq.heapify(max_heap)
+        # # Extract the top k elements
+        # result = []
+        # for _ in range(k):
+        #     freq, num = heapq.heappop(max_heap)
+        #     result.append(num)
+        # return result
 
 
 
