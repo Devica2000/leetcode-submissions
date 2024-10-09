8b555c40aba9191d043ac5918e3590eb3e46672c
Devica2000 <63335835+Devica2000@users.noreply.github.com>
2024-10-08 22:38:36 -0700

    Time: 98 ms (22.9%), Space: 16.6 MB (18.84%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,5 +1,51 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        #Time Complexity O(9^2)
+        #creating a hashSet for col, row, and square
+        #row number is the key
+        #all values within that row are the values
+        
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in rows[r] or
+                    board[r][c] in cols[c] or
+                    board[r][c] in squares[(r//3, c//3)]):
+                    return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r//3, c//3)].add(board[r][c])
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #Time Complexity - O(9^2)
         rows = collections.defaultdict(set)
         cols = collections.defaultdict(set)
         squares = collections.defaultdict(set)
