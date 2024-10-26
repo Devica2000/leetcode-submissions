14996e5208c077d90901288867d052a04bcfd5e8
Devica2000 <63335835+Devica2000@users.noreply.github.com>
2024-10-25 23:38:18 -0700

    Time: 246 ms (91.32%), Space: 38.2 MB (63.23%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -1,5 +1,32 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        
+        #make an array of pairs of position, speed
+        #use list comprehension
+        pair = [[p,s] for p, s in zip(position, speed)]
+        stack = []
+
+        #sort the pair array by position
+        #we go from right to left
+        for p,s in sorted(pair)[::-1]:
+            stack.append((target - p)/s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         pair = [[p,s] for p,s in zip(position,speed)]
         # to return the number of car fleet at the end
         stack = []
