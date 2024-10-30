2b7eb4d8cc5059db5d0690be65d34d1837cc45b7
Devica2000 <63335835+Devica2000@users.noreply.github.com>
2024-10-30 00:02:39 -0700

    Time: 133 ms (90.85%), Space: 17.8 MB (84.73%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.py
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.py
@@ -1,5 +1,21 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        res = r
+
+        while l<= r:
+            k = (l + r)//2
+            hours = 0
+            for p in piles:
+                hours += math.ceil(p/k)
+                
+            if hours <= h:
+                res = min(res, k)
+                r = k - 1
+            else:
+                l = k + 1
+
+        return res
 
 
 
@@ -10,7 +26,7 @@ class Solution:
 
 
 
-        
+
         #Time Complexity = O(log(max p) * p)
         #Given: len(piles) <= h  
         #Koko only eats at most 1 pile of bananas in 1 hour
