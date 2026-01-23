132dd54cc5b67e416679093c19dbde7a418ad067
Devica2000 <63335835+Devica2000@users.noreply.github.com>
2026-01-22 19:31:15 -0800

    Time: 11 ms (75.17%), Space: 19.5 MB (23.49%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,21 +1,21 @@
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        # if len(s) != len(t):
+        #     return False
         
-        countS, countT = {}, {}
+        # countS, countT = {}, {}
 
-        #iterate through one of the hashmaps
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
+        # #iterate through one of the hashmaps
+        # for i in range(len(s)):
+        #     countS[s[i]] = 1 + countS.get(s[i], 0)
+        #     countT[t[i]] = 1 + countT.get(t[i], 0)
 
-        for c in countS:
-            if countS[c] != countT.get(c, 0):
-                return False
+        # for c in countS:
+        #     if countS[c] != countT.get(c, 0):
+        #         return False
 
-        return True
+        # return True
         # return sorted(s) == sorted(t)
 
         # if Counter(s) == Counter(t):
