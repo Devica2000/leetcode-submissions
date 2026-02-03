0fcf20ace3f32c41cf1abfc5aaa1836904b44039
Devica2000 <63335835+Devica2000@users.noreply.github.com>
2026-02-02 23:42:46 -0800

    Time: 72 ms (49.85%), Space: 19.9 MB (5.53%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -15,7 +15,7 @@ class Codec:
             while s[j] != "#":
                 j += 1
 
-            length = s[i:j]
+            length = int(s[i:j])
             res.append(s[j + 1: j + 1 + length])
             i = j + 1 + length
 
