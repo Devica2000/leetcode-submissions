a78f9b0a2c4c703982e0b64d9ef9145a4385666c
Devica2000 <63335835+Devica2000@users.noreply.github.com>
2026-02-02 23:43:17 -0800

    Time: 62 ms (88.64%), Space: 19.7 MB (31.7%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -23,51 +23,29 @@ class Codec:
 
 
 
+    #     #use this function to create
+    #     #a single strip of all strings in list
+    #     #add length and delimiter in the encoded string
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        #use this function to create
-        #a single strip of all strings in list
-        #add length and delimiter in the encoded string
-
-        res = ""
-        for s in strs:
-            res += str(len(s)) + "#" + s
-        return res
+    #     res = ""
+    #     for s in strs:
+    #         res += str(len(s)) + "#" + s
+    #     return res
     
-    def decode(self, s:str) -> List[str]:
-        res = []
-        i = 0
+    # def decode(self, s:str) -> List[str]:
+    #     res = []
+    #     i = 0
 
-        while i < len(s):
-            j = i
-            while s[j] != "#":
-                j += 1
-            #extract the length from the encoded string
-            length = int(s[i:j])
-            res.append(s[j+1:j+1+length])
-            i = j + 1 + length
+    #     while i < len(s):
+    #         j = i
+    #         while s[j] != "#":
+    #             j += 1
+    #         #extract the length from the encoded string
+    #         length = int(s[i:j])
+    #         res.append(s[j+1:j+1+length])
+    #         i = j + 1 + length
 
-        return res
+    #     return res
 
 
 
